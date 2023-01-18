18f51739c78f14c333508554990e2fe5f63411b2
lucas-sancere <lsancere@uni-koeln.de>
2023-01-18 18:41:52 +0100

    Custom Dataset fully fixed, training working

---
+++ tools/dist_train_lucas_pudb.sh
@@ -0,0 +1,12 @@
+CONFIG=$1
+GPUS=$2
+NNODES=${NNODES:-1}
+NODE_RANK=${NODE_RANK:-0}
+PORT=${PORT:-29500}
+MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
+
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+python -m pudb \
+    $(dirname "$0")/train.py \
+    $CONFIG \
+
